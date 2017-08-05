f25ad81cdb80eb784b2e85883566f9b362470e3d
Giuan91 <dany9173@yahoo.com>
2017-08-05 22:54:36 +0300

    KilogramsOfHayProblem

---
+++ CalculateHayConsumption/CalculateHayConsumption.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculateHayConsumption
+{
+    [TestClass]
+    public class CalculateHayConsumption
+    {
+        [TestMethod]
+        public void HayConsumptionForOneDay()
+        {
+            int kilogramsOfHay = CalculateKilogramsOfHayConsumed(1, 10, 100);
+            Assert.AreEqual(0, kilogramsOfHay);
+        }
+
+        int CalculateKilogramsOfHayConsumed(int numberOfDays, int numberOfGoats, int kilogramsOfHay)
+        {
+            return 0;
+        }
+    }
+}
