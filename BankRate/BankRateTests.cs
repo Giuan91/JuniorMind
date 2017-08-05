dc947c5023cef261056fce88203047237229a054
Giuan91 <dany9173@yahoo.com>
2017-08-05 22:44:40 +0300

    BankRateProblem

--- BankRate/BankRateTests.cs
+++ BankRate/BankRateTests.cs
@@ -7,14 +7,25 @@ namespace BankRate
     public class BankRateTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void RateForFirstMonth()
         {
             decimal rate = CalculateBankRate(200, 2, 12, 1);
             Assert.AreEqual(102, rate);
         }
+
+        [TestMethod]
+        public void RateForSecondMonth()
+        {
+            decimal rate = CalculateBankRate(200, 2, 12, 2);
+            Assert.AreEqual(101, rate);
+        }
+
         decimal CalculateBankRate(decimal total, int periodInMonths, decimal interestPerYear, int currentMonth)
         {
-            return total / periodInMonths + total * interestPerYear / 12 / 100;
+            decimal principal = total / periodInMonths;
+            decimal exactInterestPerMonth = interestPerYear / 12 / 100;
+            decimal sold = total - (currentMonth - 1) * principal;
+            return principal + sold * exactInterestPerMonth; ;
         }
     }
 }
