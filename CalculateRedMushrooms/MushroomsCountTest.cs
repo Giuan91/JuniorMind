abcdd2e7d20ea2859c11c59981df0d70e29d1d09
Giuan91 <dany9173@yahoo.com>
2017-08-05 22:42:11 +0300

    MushroomProblem

--- CalculateRedMushrooms/MushroomsCountTest.cs
+++ CalculateRedMushrooms/MushroomsCountTest.cs
@@ -9,24 +9,25 @@ namespace MushroomCount
         [TestMethod]
         public void TwoTimesMoreRedMushrooms()
         {
-            int redMushrooms = CalculateNumberOfRedMushrooms(90, 30, 2);
+            int redMushrooms = CalculateNumberOfRedMushrooms(90, 2);
             Assert.AreEqual(60, redMushrooms);
         }
 
         [TestMethod]
         public void FiveTimesMoreRedMushrooms()
         {
-            int redMushrooms = CalculateNumberOfRedMushrooms(120, 20, 5);
+            int redMushrooms = CalculateNumberOfRedMushrooms(120, 5);
             Assert.AreEqual(100, redMushrooms);
         }
 
 
-        int CalculateNumberOfRedMushrooms(int totalNumberOfMushrooms, int numberOfWhiteMushrooms, int x)
+        int CalculateNumberOfRedMushrooms(int totalNumberOfMushrooms, int x)
         {
-            int red = numberOfWhiteMushrooms * x;
-            int total = (numberOfWhiteMushrooms + red);
-            return total - numberOfWhiteMushrooms;
+            int white = totalNumberOfMushrooms / (1 + x);
+
+            return white * x;
         }
 
     }
 }
+ 
