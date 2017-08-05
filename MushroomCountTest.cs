a2457fed68b3ee5001c34771778b97b0f1e38cd9
Giuan91 <dany9173@yahoo.com>
2017-08-05 03:39:23 +0300

    TwoProblemsSolved
    solved 2 problems and am trying to figure out GitHub again

---
+++ MushroomCountTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MushroomCount
+{
+    [TestClass]
+    public class MushroomCountTest
+    {
+        [TestMethod]
+        public void TwoTimesMoreRedMushrooms()
+        {
+            int redMushrooms = CalculateNumberOfRedMushrooms(90 , 30, 2);
+            Assert.AreEqual(60, redMushrooms);
+        }
+
+        [TestMethod]
+        public void FiveTimesMoreRedMushrooms()
+        {
+            int redMushrooms = CalculateNumberOfRedMushrooms(120, 20, 5);
+            Assert.AreEqual(100, redMushrooms);
+        }
+
+
+        int CalculateNumberOfRedMushrooms (int totalNumberOfMushrooms, int numberOfWhiteMushrooms, int x)
+        {
+            int red = numberOfWhiteMushrooms * x;
+            int total = (numberOfWhiteMushrooms + red);
+            return  total - numberOfWhiteMushrooms;
+        }
+
+        }
+}
