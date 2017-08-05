f9878299252cd5c9c50661264098823c50fc91eb
Giuan91 <dany9173@yahoo.com>
2017-08-05 22:02:02 +0300

    no message

---
+++ BankRate/BankRateTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankRate
+{
+    [TestClass]
+    public class BankRateTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            decimal rate = CalculateBankRate(200, 2, 12, 1);
+            Assert.AreEqual(102, rate);
+        }
+        decimal CalculateBankRate(decimal total, int periodInMonths, decimal interestPerYear, int currentMonth)
+        {
+            return total / periodInMonths + total * interestPerYear / 12 / 100;
+        }
+    }
+}
