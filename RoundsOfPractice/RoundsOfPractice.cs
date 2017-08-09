614049a0d8341077e6814cf0101c93f1b479b50b
Giuan91 <dany9173@yahoo.com>
2017-08-09 11:27:15 +0300

    AthletProblem

---
+++ RoundsOfPractice/RoundsOfPractice.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculateRoundsOfPractice
+{
+    [TestClass]
+    public class RoundsOfPractice
+    {
+        [TestMethod]
+        public void CalculateRepetitionsForFiveMaximumRepetitions()
+        {
+            int numberOfReps = CalculateNumberOfRepetitions(5);
+            Assert.AreEqual(25, numberOfReps);
+        }
+
+        [TestMethod]
+        public void CalculateRepetitionsForNineMaximumRepetitions()
+        {
+            int numberOfReps = CalculateNumberOfRepetitions(9);
+            Assert.AreEqual(81, numberOfReps);
+        }
+
+        int CalculateNumberOfRepetitions(int numberOfMaximumRepetitions)
+        {
+            int numberOfRounds = numberOfMaximumRepetitions + (numberOfMaximumRepetitions - 1);
+            int numberOfRepetitionsForFirstRounds = (numberOfMaximumRepetitions * (numberOfMaximumRepetitions + 1)) / 2;
+            int numberOfRepetitionsForSecondRounds = (numberOfMaximumRepetitions * (numberOfMaximumRepetitions - 1)) / 2;
+            int numberOfRepetitions = numberOfRepetitionsForFirstRounds + numberOfRepetitionsForSecondRounds;
+
+
+            return numberOfRepetitions;
+        }
+    }
+}
