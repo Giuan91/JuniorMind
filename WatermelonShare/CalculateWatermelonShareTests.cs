8e90f248dc618b434961cc144beedac7a505cca5
Giuan91 <dany9173@yahoo.com>
2017-08-22 21:42:19 +0300

    If equals 2

--- WatermelonShare/CalculateWatermelonShareTests.cs
+++ WatermelonShare/CalculateWatermelonShareTests.cs
@@ -18,11 +18,20 @@ namespace WatermelonShare
             StringAssert.Equals("NO", WatermelonShareCalculator(5));
         }
 
+        [TestMethod]
+        public void IfEqualsTwo()
+        {
+            StringAssert.Equals("NO", WatermelonShareCalculator(2));
+        }
+
         string WatermelonShareCalculator(int watermelonWeigtInKilograms)
         {
             if(watermelonWeigtInKilograms % 2 == 0)
             {
                 return "YES";
+            }else if(watermelonWeigtInKilograms == 2)
+            {
+                return "NO";
             }else
             {
                 return "NO";
