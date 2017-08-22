d124815bc11dc6b4b37e6b260084debf912e5685
Giuan91 <dany9173@yahoo.com>
2017-08-22 14:09:21 +0300

    Return Yes or No if watermelon cand be shared in even pairs

---
+++ WatermelonShare/CalculateWatermelonShareTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WatermelonShare
+{
+    [TestClass]
+    public class CalculateWatermelonShareTests
+    {
+        [TestMethod]
+        public void PrimeNumberOfKilograms()
+        {
+            StringAssert.Equals("YES", WatermelonShareCalculator(8));
+        }
+
+        [TestMethod]
+        public void OddNumberOfKilograms()
+        {
+            StringAssert.Equals("NO", WatermelonShareCalculator(5));
+        }
+
+        string WatermelonShareCalculator(int watermelonWeigtInKilograms)
+        {
+            if(watermelonWeigtInKilograms % 2 == 0)
+            {
+                return "YES";
+            }else
+            {
+                return "NO";
+            }
+            
+        }
+    }
+}
