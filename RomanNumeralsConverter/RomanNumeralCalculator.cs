9e23dcdce37246f193546b05b4ae36d21416540f
Giuan91 <dany9173@yahoo.com>
2017-08-29 13:38:22 +0300

    Defining the first test

---
+++ RomanNumeralsConverter/RomanNumeralCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RomanNumeralsConverter
+{
+    [TestClass]
+    public class RomanNumeralCalculator
+    {
+        [TestMethod]
+        public void ConvertNine()
+        {
+            Assert.AreEqual("IX", NumberConverter(9));
+        }
+        string NumberConverter(int numberToBeConverted)
+        {
+            string romanValue = null;
+            return romanValue;
+        }
+    }
+}
