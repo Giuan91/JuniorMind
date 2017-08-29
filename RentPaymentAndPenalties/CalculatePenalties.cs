abfb0ae4db953cd3372af713f8f95b1e2ba0a0d1
Giuan91 <dany9173@yahoo.com>
2017-08-29 12:54:56 +0300

    First test succces

--- RentPaymentAndPenalties/CalculatePenalties.cs
+++ RentPaymentAndPenalties/CalculatePenalties.cs
@@ -9,12 +9,28 @@ namespace RentPaymentAndPenalties
         [TestMethod]
         public void OverdueForTwoDays()
         {
-            Assert.AreEqual(4, PenaltiesCalculator(100, 2));
+            Assert.AreEqual(104, PenaltiesCalculator(100, 2));
         }
 
         decimal PenaltiesCalculator(int rentPerMonth, int numberOfDaysOverdue)
         {
-            return 0;
+            decimal rentPlusPenalty = rentPerMonth;
+            
+                if (1 <= numberOfDaysOverdue && numberOfDaysOverdue < 11)
+                {
+                    rentPlusPenalty = ((2 * rentPerMonth / 100) * numberOfDaysOverdue) + rentPerMonth;
+                   
+                } else if (11 <= numberOfDaysOverdue && numberOfDaysOverdue < 31)
+                {
+                    rentPlusPenalty = ((5 * rentPerMonth / 100) * numberOfDaysOverdue) + rentPerMonth;
+
+                } else if (31 <= numberOfDaysOverdue && numberOfDaysOverdue < 41)
+                {
+                    rentPlusPenalty = ((10 * rentPerMonth / 100) * numberOfDaysOverdue) + rentPerMonth;
+                }
+
+
+            return rentPlusPenalty; 
         }
            
     }
