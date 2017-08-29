bee8efc4fccc62bafe5641d98764c967485e2a63
Giuan91 <dany9173@yahoo.com>
2017-08-29 12:12:38 +0300

    FizzBuzz Problem completed

--- OldMcDonaldsFarm/AreaCalculator.cs
+++ OldMcDonaldsFarm/AreaCalculator.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace OldMcDonaldsFarm
 {
     [TestClass]
-    public class UnitTest1
+    public class AreaCalculator
     {
         [TestMethod]
         public void TestMethod1()
