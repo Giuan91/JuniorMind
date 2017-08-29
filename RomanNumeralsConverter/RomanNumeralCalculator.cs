dab44f588e93e6a7970976f84eb35ca635142dc3
Giuan91 <dany9173@yahoo.com>
2017-08-29 15:36:24 +0300

    Complete

--- RomanNumeralsConverter/RomanNumeralCalculator.cs
+++ RomanNumeralsConverter/RomanNumeralCalculator.cs
@@ -7,14 +7,88 @@ namespace RomanNumeralsConverter
     public class RomanNumeralCalculator
     {
         [TestMethod]
+        public void ConvertFour()
+        {
+            Assert.AreEqual("IV", NumberConverter(4));
+        }
+
+        [TestMethod]
+        public void ConvertEight()
+        {
+            Assert.AreEqual("VIII", NumberConverter(8));
+        }
+
+        [TestMethod]
         public void ConvertNine()
         {
             Assert.AreEqual("IX", NumberConverter(9));
         }
+
+
+        [TestMethod]
+        public void ConvertFifteen()
+        {
+            Assert.AreEqual("XV", NumberConverter(15));
+        }
+
+
+        [TestMethod]
+        public void ConvertThirty()
+        {
+            Assert.AreEqual("XXX", NumberConverter(30));
+        }
+
+
+        [TestMethod]
+        public void ConvertThirtyNine()
+        {
+            Assert.AreEqual("XXXIX", NumberConverter(39));
+        }
+
+
+        [TestMethod]
+        public void ConvertFortyOne()
+        {
+            Assert.AreEqual("XLI", NumberConverter(41));
+        }
+
+
+        [TestMethod]
+        public void ConvertFortyFive()
+        {
+            Assert.AreEqual("XLV", NumberConverter(45));
+        }
+
+
+        [TestMethod]
+        public void ConvertNinetyNine()
+        {
+            Assert.AreEqual("XCIX", NumberConverter(99));
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException),"Out of bounds")]
+        public void ConvertOneHundredAndTwo()
+        {
+            AssertFailedException.Equals("Out of bounds", NumberConverter(102));
+        }
+
         string NumberConverter(int numberToBeConverted)
         {
-            string romanValue = null;
-            return romanValue;
+            if (numberToBeConverted < 0 || numberToBeConverted > 100) throw new ArgumentOutOfRangeException("Out of bounds");
+            if (numberToBeConverted < 1) return string.Empty;
+            if (numberToBeConverted == 100) return "C" + NumberConverter(numberToBeConverted - 100);
+            if (numberToBeConverted >= 90) return "XC" + NumberConverter(numberToBeConverted - 90);
+            if (numberToBeConverted >= 50) return "L" + NumberConverter(numberToBeConverted - 50);
+            if (numberToBeConverted >= 40) return "XL" + NumberConverter(numberToBeConverted - 40);
+            if (numberToBeConverted >= 10) return "X" + NumberConverter(numberToBeConverted - 10);
+            if (numberToBeConverted >= 9) return "IX" + NumberConverter(numberToBeConverted - 9);
+            if (numberToBeConverted >= 5) return "V" + NumberConverter(numberToBeConverted - 5);
+            if (numberToBeConverted >= 4) return "IV" + NumberConverter(numberToBeConverted - 4);
+            if (numberToBeConverted >= 1) return "I" + NumberConverter(numberToBeConverted - 1);
+            throw new ArgumentOutOfRangeException("Out Of bounds");
+            
         }
     }
 }
