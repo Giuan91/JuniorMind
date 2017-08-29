563da4d881c9d5acc2151a8e7c9abf6beadef93f
Giuan91 <dany9173@yahoo.com>
2017-08-29 12:28:58 +0300

    OverdueCalculation

---
+++ RentPaymentAndPenalties/CalculatePenalties.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RentPaymentAndPenalties
+{
+    [TestClass]
+    public class CalculatePenalties
+    {
+        [TestMethod]
+        public void OverdueForTwoDays()
+        {
+            Assert.AreEqual(4, PenaltiesCalculator(100, 2));
+        }
+
+        decimal PenaltiesCalculator(int rentPerMonth, int numberOfDaysOverdue)
+        {
+            return 0;
+        }
+           
+    }
+}
