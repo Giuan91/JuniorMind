49d91bec6b00c60e969e7acf062f68a261514717
Giuan91 <dany9173@yahoo.com>
2017-09-12 19:01:54 +0300

    Created Test Method

---
+++ ChessBoard/SquaresCalculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChessBoard
+{
+    [TestClass]
+    public class SquaresCalculator
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(92, ChessBoardTotalSquaresCalculator(8, 8));
+        }
+        int ChessBoardTotalSquaresCalculator(int m, int n)
+        {
+            return 0;
+        }
+    }
+}
