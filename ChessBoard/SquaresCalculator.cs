e71d199d399ce2471e78d80c3ac844843311ee8e
Giuan91 <dany9173@yahoo.com>
2017-09-15 11:58:59 +0300

    added problem solving method and secont test method

--- ChessBoard/SquaresCalculator.cs
+++ ChessBoard/SquaresCalculator.cs
@@ -9,11 +9,23 @@ namespace ChessBoard
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.AreEqual(92, ChessBoardTotalSquaresCalculator(8, 8));
+            Assert.AreEqual(204, ChessBoardTotalSquaresCalculator(8));
         }
-        int ChessBoardTotalSquaresCalculator(int m, int n)
+
+        [TestMethod]
+        public void TestMethod2()
         {
-            return 0;
+            Assert.AreEqual(385, ChessBoardTotalSquaresCalculator(10));
+        }
+        int ChessBoardTotalSquaresCalculator(int unit)
+        {
+            int i, square, sum = 0;
+            for (i = 1; i <= unit; i++)
+            {
+                square = i * i;
+                sum += square;
+            } 
+            return sum;
         }
     }
 }
---
+++ PrefixFinder/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PrefixFinder
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            CollectionAssert.AreEqual("semic", PrefixFinder());
+        }
+
+        string PrefixFinder(string prefix)
+        {
+            return prefix;
+        }
+    }
+}
