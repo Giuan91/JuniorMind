7fc5eeb5f0840d0760191bd935e4ac5e5210ce6b
Giuan91 <dany9173@yahoo.com>
2017-09-15 12:36:39 +0300

    created methodn asnd second test

---
+++ PrefixFinder/PrefixFind.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PrefixFinder
+{
+    [TestClass]
+    public class PrefixFind
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            StringAssert .StartsWith("semico", PrefixFinder("semiconductor", "semicolon"));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            StringAssert.StartsWith("aaa", PrefixFinder("aaab", "aaaaaabbaa"));
+        }
+
+        string PrefixFinder(string phraseOne, string phraseTwo)
+        {
+            int i = 0;
+            while (phraseOne[i] == phraseTwo[i])
+            {
+                i++;
+            }
+            string prefix = phraseOne.Substring(0, i);
+            return prefix;
+        }
+    }
+}
