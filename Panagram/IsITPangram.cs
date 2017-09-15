1acdaae2918f676e6d09cce0059d9f76b5b69ad0
Giuan91 <dany9173@yahoo.com>
2017-09-15 13:34:34 +0300

    created first test method

---
+++ Panagram/IsITPangram.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pangram
+{
+    [TestClass]
+    public class IsItPangram
+    {
+        [TestMethod]
+        public void FirstPhrase()
+        {
+            Assert.AreEqual(true, Pangram("The quick brown fox jumps over the lazy dog"));
+        }
+
+
+        bool Pangram( string phrase)
+        {
+            return false;
+        }
+    }
+}
