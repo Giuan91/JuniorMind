a2745e19ea5039edbca90a9e9f4d562c05f92afa
Giuan91 <dany9173@yahoo.com>
2017-09-17 16:50:37 +0300

    done

--- ExelColumnNumber/FindColumn.cs
+++ ExelColumnNumber/FindColumn.cs
@@ -12,10 +12,33 @@ namespace ExelColumnNumber
             Assert.AreEqual("A", ColumnPosition(1));
         }
 
+        [TestMethod]
+        public void TestForDoubleLettersColumn()
+        {
+            Assert.AreEqual("AA", ColumnPosition(27));
+        }
+
+        [TestMethod]
+        public void TestForThreeLettersColumn()
+        {
+            Assert.AreEqual("AAA", ColumnPosition(703));
+        }
+
         string ColumnPosition(int columnNumber)
         {
-            return null;
+            
+            string columnName = String.Empty;
+            
+            while (columnNumber > 0)
+            {
+                int modulo = (columnNumber - 1) % 26;
+                columnName = Convert.ToChar(65 + modulo).ToString() + columnName;
+                columnNumber = (columnNumber - modulo) / 26;
+            }
+
+            return columnName;
         }
+    
 
     }
 }
